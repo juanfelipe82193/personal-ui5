844156edccce46fb95a2e77698a8af6d00c688ab
juanfelipe82193 <juan19_93@hotmail.com>
2020-07-07 20:39:19 -0500

    Updates

---
+++ precache-manifest.3c2d174dcef57e47ecefe1044d4293aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc205e91f9507ccf6ee5701ecfff80b6",
+    "url": "/personal-ui5/index.html"
+  },
+  {
+    "revision": "cb6c26ec43005baab684",
+    "url": "/personal-ui5/static/css/main.8917f35e.chunk.css"
+  },
+  {
+    "revision": "2f97e17614c8542796fb",
+    "url": "/personal-ui5/static/js/2.92a7dd4a.chunk.js"
+  },
+  {
+    "revision": "20f161f66fff2df47b6b5004f97acb5d",
+    "url": "/personal-ui5/static/js/2.92a7dd4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb6c26ec43005baab684",
+    "url": "/personal-ui5/static/js/main.d2698dcd.chunk.js"
+  },
+  {
+    "revision": "705bb0d9eef7ac4f226f",
+    "url": "/personal-ui5/static/js/runtime-main.e2af46b1.js"
+  }
+]);
